9a25f8803a733235ff9badea8f69e2fe2d436ebb
wangcheng <811401531@qq.com>
2018-03-20 09:51:48 +0800

    删除bin-debug文件夹以及编辑器工程配置文件

--- bin-debug/zero/Global.js
+++
@@ -1,7 +0,0 @@
-var zero;
-(function (zero) {
-    zero.sceneMgr = zero.SceneMgr.getInstance();
-    zero.layerMgr = zero.LayerMgr.getInstance();
-    zero.loadingMgr = zero.LoadingMgr.getInstance();
-    zero.notificationCenter = zero.NotificationCenter.getInstance();
-})(zero || (zero = {}));
